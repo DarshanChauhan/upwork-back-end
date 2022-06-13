111baa549cc3f1288907b0104f31f93510e13b6b
Darshan <darshanchauhan7866@gmail.com>
2022-06-13 15:25:57 +0530

    trying o fix and all table added

--- server.js
+++ server.js
@@ -1,35 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+app.use(cors());
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const dotenv = require("dotenv");
 require("./model/index");
-const corsOpts = {
-  origin: "*",
-
-  methods: ["GET", "POST"],
-
-  allowedHeaders: ["Content-Type"],
-};
-
-app.use(cors(corsOpts));
-
-app.all("/*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
-
-app.use(express.static("client"));
 app.use(express.json());
 const route = require("./routes/route");
 app.use(route);
 dotenv.config();
-
-// PORT
-
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT} `);
 });
+// const corsOpts = {
+//   origin: "*",
+
+//   methods: ["GET", "POST"],
+
+//   allowedHeaders: ["Content-Type"],
+// };
+
+// app.use(function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
+
+// app.all("/*", function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+// });
+
+// app.use(express.static("client"));
+
+// PORT
